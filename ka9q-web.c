dcff5fd2bb4efa798ac798fb7df51aa4d4fdc9c2
Scott Newell <github@n5tnl.com>
2025-02-02 16:48:03 +0000

    Update versions

--- ka9q-web.c
+++ ka9q-web.c
@@ -40,7 +40,7 @@
 #include "radio.h"
 #include "config.h"
 
-const char *webserver_version = "2.57";
+const char *webserver_version = "2.58";
 
 // no handlers in /usr/local/include??
 onion_handler *onion_handler_export_local_new(const char *localpath);
