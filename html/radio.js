9965e9628eeeebb03e7984e81a162bc1a4feac75
wayne roth <github1@wa2n.net>
2025-03-09 23:10:07 -0400

    Noise level to noise power label change

--- html/radio.js
+++ html/radio.js
@@ -683,11 +683,7 @@ var noisePower = updateSMeter(power,noise_density_audio,bw,spectrum.maxHold);
   document.getElementById('rf_cal').innerHTML = "RF lev cal: " + rf_level_cal.toFixed(1) + " dB";
   document.getElementById('rf_agc').innerHTML = (rf_agc==1 ? "RF AGC: enabled" : "RF AGC: disabled");
   document.getElementById('if_power').innerHTML = "A/D: " + if_power.toFixed(1) + " dBFS";
-
-
-  document.getElementById('noise_density').innerHTML = `N<sub>0</sub>: ${noise_density_audio.toFixed(1)} dBmJ (audio), noise level at BW ${bw}: ${noisePower.toFixed(1)} dBm`;
-
-
+  document.getElementById('noise_density').innerHTML = `N<sub>0</sub>: ${noise_density_audio.toFixed(1)} dBmJ (audio), Noise power at BW ${bw}: ${noisePower.toFixed(1)} dBm`;
   document.getElementById('bins').textContent = `Bins: ${binCount}`;
   document.getElementById('hz_per_bin').textContent = `Bin width: ${binWidthHz} Hz`;
   document.getElementById('blocks').innerHTML = "Blocks/poll: " + blocks_since_last_poll.toString();
