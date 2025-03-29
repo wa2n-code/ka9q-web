32e983940d940921d2c004be6d3e1fa6a87d3026
wayne roth <github1@wa2n.net>
2025-03-28 20:24:12 -0400

    Stopped writing to cursor_data if the cursor is not active and cleared the text it generated.

--- html/radio.js
+++ html/radio.js
@@ -777,7 +777,11 @@ var noisePower = updateSMeter(power,noise_density_audio,bw,spectrum.maxHold);
   if (webpage_version != webserver_version)
     document.getElementById('webserver_version').innerHTML += " <b>Warning: version mismatch!</b>";
 
-  document.getElementById("cursor_data").innerHTML = "<br>Tune: " + level_to_string(spectrum.frequency) + "<br>Cursor: " + level_to_string(spectrum.cursor_freq);
+  if(spectrum.cursor_active)
+    document.getElementById("cursor_data").innerHTML = "<br>Tune: " + level_to_string(spectrum.frequency) + "<br>Cursor: " + level_to_string(spectrum.cursor_freq);
+  else
+    document.getElementById("cursor_data").innerHTML = ""; // clear the cursor data if it's not active
+  
   document.getElementById("spare2").textContent = `low: ${lowHz / 1000.0} kHz, high: ${highHz / 1000.0} kHz, center: ${centerHz / 1000.0} kHz, tune: ${frequencyHz / 1000.0} kHz`;
 
   // Show reordered info into ge_data left table column 1
